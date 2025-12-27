16972bad3be9d1e70dc45c076009a0a24786601d
Abhishek Rangani <abhie2005@Mac.lan>
2025-12-27 14:53:11 -0800

    Added icon to the website

--- frontend/src/Auth.js
+++ frontend/src/Auth.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Auth.css';
-import iconImage from '../Pictures/icon_clock_in_clock_out.png';
+import iconImage from './Pictures/icon_clock_in_clock_out.png';
 
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://fullstack-time-tracking-app-backend.onrender.com/api';
 
